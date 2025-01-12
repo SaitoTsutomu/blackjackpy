ff5249e3542d6927c5569f12d4b46638c0b9cb9a
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2025-01-12 22:37:13 +0900

    upd:pytest

--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -9,13 +9,16 @@ from blackjackpy import GameMaster
 
 
 def ask(player_answers: str) -> Callable[[], str]:
-    return lambda: next(repeat_last(player_answers))
+    answers = repeat_last(player_answers)
+    return lambda: next(answers)
 
 
 expected1 = """\
-    Player(20):  J(D) K(D)
+    Player(19):  J(D) 9(D)
     Dealer(--):  Q(D) *(*)
-    Player(20):  J(D) K(D)
+    Player(20):  J(D) 9(D) A(D)
+    Dealer(--):  Q(D) *(*)
+    Player(20):  J(D) 9(D) A(D)
     Dealer(19):  Q(D) 9(D)
     You win.
 """
@@ -30,10 +33,17 @@ expected3 = """\
     Player(20):  J(D) K(D)
     Dealer(--):  Q(D) *(*)
     Player(20):  J(D) K(D)
+    Dealer(17):  Q(D) 7(D)
+    You win.
+"""
+expected4 = """\
+    Player(20):  J(D) K(D)
+    Dealer(--):  Q(D) *(*)
+    Player(20):  J(D) K(D)
     Dealer(21):  Q(D) 6(D) 5(D)
     You lose.
 """
-expected4 = """\
+expected5 = """\
     Player(20):  J(D) K(D)
     Dealer(--):  Q(D) *(*)
     Player(21):  J(D) K(D) A(D)
@@ -45,10 +55,11 @@ expected4 = """\
 @pytest.mark.parametrize(
     ("cards", "player_answers", "expected"),
     [
-        ([10, 11, 12, 8], "n", dedent(expected1)),
-        ([10, 11, 12, 8, 0], "yn", dedent(expected2)),
-        ([10, 11, 12, 5, 4], "n", dedent(expected3)),
-        ([10, 11, 12, 5, 0, 4], "yn", dedent(expected4)),
+        ([10, 11, 8, 8, 0], "yn", dedent(expected1)),
+        ([10, 11, 12, 8, 0], "y", dedent(expected2)),
+        ([10, 11, 12, 6], "n", dedent(expected3)),
+        ([10, 11, 12, 5, 4], "n", dedent(expected4)),
+        ([10, 11, 12, 5, 0, 4], "yn", dedent(expected5)),
     ],
 )
 def test_game(capsys, cards: list[int], player_answers: str, expected: str):
